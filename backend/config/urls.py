6ee8cf7ef081c310213b851757bcc001062dde9b
Dev-KingSnyf <sihnoyann@gmail.com>
2026-06-12 09:11:18 +0100

    okays

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,6 +4,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from api.serializers import CustomTokenSerializer
 from api.views import RegisterView
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1 style='font-family: sans-serif; text-align: center; margin-top: 50px;'>🚀 L'API Django fonctionne parfaitement !</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +18,5 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', home),
 ]
